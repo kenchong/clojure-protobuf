fa58b5667dda040adbf63bee91eac3afa2b7016d
Justin Balthrop <git@justinbalthrop.com>
2011-12-12 14:03:32 -0800

    update to non-snapshot

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :dependencies [[clojure "1.2.0"]
                  [ordered-set "0.2.2"]
-                 [useful "0.7.4-alpha4"]]
+                 [useful "0.7.5"]]
   :test-dependencies [[gloss "0.2.0-rc1"]
-                      [io "0.1.0-alpha1"]]
-  :cake-plugins [[cake-protobuf "0.5.0-beta1"]]
+                      [io "0.1.0"]]
+  :cake-plugins [[cake-protobuf "0.5.0"]]
   :jar-files ["proto"])
